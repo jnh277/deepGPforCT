56dd528bc350091456f58117fc881cdd8c33030e
Johannes <>
2018-12-14 22:49:08 +0100

    Add GP_SE class that allows for gp regressions with squared exponential cov and data dimension greater than 1 add example_NN_GP_step that recreates the deep GP step example

---
+++ example_NN_GP_Step.py
@@ -0,0 +1,95 @@
+import math
+import torch
+import gp_regression as gpr
+import torch.nn as nn
+import gpytorch
+from matplotlib import pyplot as plt
+
+from math import floor
+
+n = 300
+train_x = torch.Tensor(n, 1)
+train_x[:, 0] = torch.linspace(0, 1, n)
+train_y = 0.25*torch.sin(torch.squeeze(train_x, 1) * (3 * math.pi))
+train_y[torch.squeeze(train_x, 1) > 0.5] = train_y[torch.squeeze(train_x, 1) > 0.5] + 1
+# train_y[torch.squeeze(train_x, 1) <= 0.5] = train_y[torch.squeeze(train_x, 1) <= 0.5]
+train_y = train_y + torch.randn(train_y.size()) * 0.05
+
+test_x = torch.Tensor(100, 1)
+test_x[:, 0] = torch.linspace(0, 1, 100)
+
+
+data_dim = train_x.size(-1)
+
+
+
+
+
+class DeepGP(torch.nn.Module):
+    def __init__(self):
+        """
+        In the constructor we instantiate two nn.Linear modules and assign them as
+        member variables.
+        """
+        super(DeepGP, self).__init__()
+        self.linear1 = torch.nn.Linear(1, 5)
+        self.tanh1 = torch.nn.Tanh()
+        self.linear2 = torch.nn.Linear(5, 2)
+        self.tanh2 = torch.nn.Tanh()
+        self.gp = gpr.GP_SE(sigma_f=1.0, lengthscale=[1, 1], sigma_n=1)
+
+    def forward(self, x_train, y_train, x_test=None):
+        """
+        In the forward function we accept a Tensor of input data and we must return
+        a Tensor of output data. We can use Modules defined in the constructor as
+        well as arbitrary operators on Tensors.
+        """
+        h = self.linear1(x_train)
+        h = self.tanh1(h)
+        h = self.linear2(h)
+        h = self.tanh2(h)
+        out = self.gp(x_train,y_train,x_test)
+        return out
+
+
+deepGP = DeepGP()
+
+nLL = gpr.NegMarginalLogLikelihood()  # this is the loss function
+
+optimizer = torch.optim.Adam([
+    {'params': deepGP.parameters()},
+], lr=0.005)
+
+training_iterations = 300
+
+
+def train():
+    for i in range(training_iterations):
+        # Zero backprop gradients
+        optimizer.zero_grad()
+        # Get output from model
+        c, v = deepGP(train_x, train_y)
+        # Calc loss and backprop derivatives
+        loss = nLL(train_y, c, v)
+        loss.backward()
+        print('Iter %d/%d - Loss: %.3f' % (i + 1, training_iterations, loss.item()))
+        optimizer.step()
+
+
+train()
+
+# now make predictions
+test_f, cov_f = deepGP(train_x,train_y,test_x)
+
+with torch.no_grad():
+    fplot, ax = plt.subplots(1, 1, figsize=(4, 3))
+    # Plot training data as black stars
+    ax.plot(train_x.numpy(), train_y.numpy(), 'k*')
+    upper = torch.squeeze(test_f, 1) + cov_f.pow(0.5)
+    lower = torch.squeeze(test_f, 1) - cov_f.pow(0.5)
+    # plot predictions
+    ax.plot(test_x.numpy(), test_f.numpy(), 'b')
+    ax.fill_between(torch.squeeze(test_x,1).numpy(), lower.numpy(), upper.numpy(), alpha=0.5)
+    ax.set_ylim([-2, 2])
+    ax.legend(['Observed Data', 'Mean', 'Confidence'])
+    plt.show()
--- gp_regression.py
+++ gp_regression.py
@@ -2,6 +2,61 @@ import torch.nn as nn
 import torch
 
 
+class GP_SE(nn.Module):
+    def __init__(self, sigma_f, lengthscale, sigma_n):
+        super(GP_SE, self).__init__()
+        # nn.Parameter is a special kind of Tensor, that will get
+        # automatically registered as Module's parameter once it's assigned
+        # as an attribute. Parameters and buffers need to be registered, or
+        # they won't appear in .parameters() (doesn't apply to buffers), and
+        # won't be converted when e.g. .cuda() is called. You can use
+        # .register_buffer() to register buffers.
+        # nn.Parameters require gradients by default.
+        self.sigma_f = nn.Parameter(torch.Tensor([sigma_f]))
+        self.lengthscale = nn.Parameter(torch.as_tensor(lengthscale, dtype=torch.float))
+        self.sigma_n = nn.Parameter(torch.Tensor([sigma_n]))
+        self.training = True        # initially we want to be in training mode
+
+    # the predict forward function
+    def forward(self, x_train, y_train, x_test=None):
+        # See the autograd section for explanation of what happens here.
+        n = x_train.size(0)
+        p = x_train.size(-1)
+        d = torch.zeros(n, n)
+        for i in range(p):
+            d += 0.5*(x_train[:,i].unsqueeze(1) - x_train[:,i].unsqueeze(0)).pow(2)/self.lengthscale[i].pow(2)
+
+        kyy = self.sigma_f.pow(2)*torch.exp(-d) + self.sigma_n.pow(2) * torch.eye(n)
+        c = torch.cholesky(kyy, upper=True)
+        # v = torch.potrs(y_train, c, upper=True)
+        v, _ = torch.gesv(y_train, kyy)
+        if x_test is None:
+            out = (c, v)
+
+        if x_test is not None:
+            with torch.no_grad():
+                ntest = x_test.size(0)
+                d = torch.zeros(ntest, n)
+                for i in range(p):
+                    d += 0.5 * (x_test[:, i].unsqueeze(1) - x_train[:, i].unsqueeze(0)).pow(2) / self.lengthscale[i].pow(2)
+                kfy = self.sigma_f.pow(2)*torch.exp(-d)
+                # solve
+                f_test = kfy.mm(v)
+                tmp = torch.potrs(kfy.t(), c, upper=True)
+                tmp = torch.sum(kfy * tmp.t(), dim=1)
+                cov_f = self.sigma_f.pow(2) - tmp
+            out = (f_test, cov_f)
+        return out
+
+
+    def extra_repr(self):
+        # (Optional)Set the extra information about this module. You can test
+        # it by printing an object of this class.
+        return 'sigma_f={}, lengthscale={}, sigma_n={}'.format(
+            self.sigma_f.item(), self.lengthscale.item(), self.sigma_n.item()
+        )
+
+
 class GP_1D(nn.Module):
     def __init__(self, sigma_f, lengthscale, sigma_n):
         super(GP_1D, self).__init__()
