216860f921abfef36d5fa05fc763ec8f6edf6d37
Johannes <>
2019-03-09 13:07:48 +1100

    slight modifications and loading of other examples

--- load_and_display_phantom_data.py
+++ load_and_display_phantom_data.py
@@ -1,8 +1,9 @@
 import scipy.io as sio
 from matplotlib import pyplot as plt
-from matplotlib import cm
-# data = sio.loadmat('../matlab/phantom_data.mat')
-data = sio.loadmat('/Users/johannes/Dropbox/deepGPforCT/matlab/phantom_data.mat')
+
+# data = sio.loadmat('/Users/johannes/Dropbox/deepGPforCT/matlab/phantom_data.mat')
+# data = sio.loadmat('/Users/johannes/Dropbox/deepGPforCT/matlab/circle_data.mat')
+data = sio.loadmat('/Users/johannes/Dropbox/deepGPforCT/matlab/circle_square_data.mat')
 
 im = data['im']                     # original image
 X = data['X']                       # X values of the original image
@@ -18,13 +19,16 @@ fbp_result = data['fbp_result']     # result from applying filtered back project
 # plot the original image
 fplot, ax = plt.subplots(2, 2, figsize=(27,15))
 h1 = ax[0,0].pcolor(X,Y,im)
+h1.set_clim(0,1)
 # plt.title('Original image')
 
 # plot the measurement data
 h2 = ax[0,1].pcolor(theta, Xp, R)
+h2.set_clim(0,1)
 
+# plot the reconstruction resulst from using iradon in matlab
 h3 = ax[1,0].pcolor(X,Y,fbp_result)
-
+h3.set_clim(0,1)
 
 plt.show()
 
